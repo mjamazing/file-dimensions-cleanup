e10c898a20b1b90e5331db620b08e703717cf1db
Jason Grace <jason@thegracefamily.net>
2020-08-09 13:47:49 -0400

    Convert to Python 3

--- dimensions-cleanup.py
+++ dimensions-cleanup.py
@@ -4,10 +4,11 @@ import os.path
 
 PATH = sys.argv[1]
 
-def visit(arg, dirname, filenames):
-  for filename in filenames:
+os.chdir(PATH)
+for root, dirs, files in os.walk(".", topdown = False):
+   for filename in files:
     if re.search("\d{2,4}x\d{2,4}", filename):
-      print dirname + "\\" + filename
+      print(os.path.join(root,filename))
       # os.remove(dirname + "\\" + filename)
-
-os.path.walk(PATH, visit, None)
+  #  for name in dirs:
+  #     print(os.path.join(root, name))
