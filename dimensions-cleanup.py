1727717cf056cd8b3f744f28078a0cba09340ad4
Jason Grace <jason@thegracefamily.net>
2020-08-09 13:48:36 -0400

    Update dimensions-cleanup.py
    Typo

--- dimensions-cleanup.py
+++ dimensions-cleanup.py
@@ -9,6 +9,6 @@ for root, dirs, files in os.walk(".", topdown = False):
    for filename in files:
     if re.search("\d{2,4}x\d{2,4}", filename):
       print(os.path.join(root,filename))
-      # os.remove(dirname + "\\" + filename)
+      # os.remove(root + "\\" + filename)
   #  for name in dirs:
   #     print(os.path.join(root, name))
